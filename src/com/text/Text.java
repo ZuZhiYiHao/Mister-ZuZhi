d372f2a14d7d0fdda09a2a7e24519e6050267145
赵伟天 <tiantian@qq.com>
2016-08-01 14:07:01 +0800

    计时

--- src/com/text/Text.java
+++ src/com/text/Text.java
@@ -9,7 +9,7 @@ public class Text {
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
 		System.out.println("可以了？更新一下！！。。。cbvbcv");
-		System.out.println("为什么!！");
+		System.out.println("为什么！！");
 		
 		
 		DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
